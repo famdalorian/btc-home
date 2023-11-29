4595f399fb6ef26f17da8fa75ff8a09da3a325b8
Jordan W <famdalorian@proton.me>
2023-11-29 17:53:34 +0000

    2/3 pod links in, on to store

--- src/components/PodcastHeroSection.jsx
+++ src/components/PodcastHeroSection.jsx
@@ -17,12 +17,12 @@ function PodcastHeroSection() {
           </a>
         </div>
         <div className="service-link">
-          <a href="https://link-to-your-spotify" target="_blank" rel="noopener noreferrer">
+          <a href="https://open.spotify.com/show/3x0iqkD2fOUfj43Mn63eEx" target="_blank" rel="noopener noreferrer">
             <i className="TopIcon fab fa-spotify"></i>
           </a>
         </div>
         <div className="service-link">
-          <a href="https://link-to-your-google-podcast" target="_blank" rel="noopener noreferrer">
+          <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5wb2RjYXN0bGUuYWkvY2ZiNmE4MTQtOGFjNy00MjZhLTg4Y2EtOTliYTQ5MDljNGJjLnJzcw" target="_blank" rel="noopener noreferrer">
             <i className="TopIcon fab fa-google"></i>
           </a>
         </div>
